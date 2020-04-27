f16822f8a5a5b6c72d6ec878dac6afb5d08b17d7
youkan <youkan94@gmail.com>
2020-04-28 02:09:31 +0900

    style : prettier + eslint

--- src/FSpyCamera.ts
+++ src/FSpyCamera.ts
@@ -1,17 +1,14 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 // import axios from 'axios';
 
-import {
-  FSpyCameraJson, FourElemArray,
-} from './type';
+import { FSpyCameraJson, FourElemArray } from './type';
 
 import AsyncFunctions from './AsyncFunctions';
 
 import { getType } from './utils/getType';
 import { getExt } from './utils/getExt';
 
-export class FSpyCamera{
-
+export class FSpyCamera {
   /**
    * fSpyから取得したjsonオブジェクトを格納
    */
@@ -23,7 +20,7 @@ export class FSpyCamera{
   public inputData: string | FSpyCameraJson;
 
   /**
-   * fSpyから取得データのアスペクトを収納 
+   * fSpyから取得データのアスペクトを収納
    */
   protected fspyImageRatio: number;
 
@@ -38,10 +35,10 @@ export class FSpyCamera{
   public camera: THREE.PerspectiveCamera;
   protected canvasWidth: number;
   protected canvasHeight: number;
-  public callback: ( thisObject : this ) => any;
+  public callback: (thisObject: this) => any;
   protected cameraFov: number;
   public options: object;
-  public targetCanvas : any;
+  public targetCanvas: any;
   protected cameraTransforms: [
     FourElemArray<number>,
     FourElemArray<number>,
@@ -51,12 +48,9 @@ export class FSpyCamera{
   public canvas: any;
   protected jsonType: string;
   protected initCameraAspect: number;
-  
 
-  constructor( canvasElement: any , options: object = {} ) {
-
-
-    this.inputData = "";
+  constructor(canvasElement: any, options: object = {}) {
+    this.inputData = '';
     this.fspyImageRatio = 0;
     this.fSpyCameraData = null;
     this.rotationMatrix = new THREE.Matrix4();
@@ -70,16 +64,16 @@ export class FSpyCamera{
     this.winWidth = window.innerWidth;
 
     /**
-     * ウィンドウの高さ 
+     * ウィンドウの高さ
      * @type {number}
      */
     this.winHeight = window.innerHeight;
 
     /**
-     * windowの幅と高さの比率をここに収納 
+     * windowの幅と高さの比率をここに収納
      * @type {number}
      */
-      this.winRatio = this.winWidth / this.winHeight;
+    this.winRatio = this.winWidth / this.winHeight;
 
     /**
      * three.jsのカメラ
@@ -95,10 +89,10 @@ export class FSpyCamera{
      * fSpyから取得したデータのうちのcameraTransform.rowsが入ります
      */
     this.cameraTransforms = [
-      [0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0],
-      [0,0,0,0]
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
     ];
 
     this.options = options;
@@ -125,38 +119,35 @@ export class FSpyCamera{
 
     this.canvasWidth = window.innerWidth;
     this.canvasHeight = 500;
-
   }
 
-  load(  jsonPathOrjsonData: string | FSpyCameraJson , callback : ( thisObject : object ) => any  ): void {
-
+  load(jsonPathOrjsonData: string | FSpyCameraJson, callback: (thisObject: object) => any): void {
     this.inputData = jsonPathOrjsonData;
     this.callback = callback;
 
-    if( this.jsonType === "string" && typeof this.inputData === "string" ){
-      const ext = getExt( this.inputData );
-      if( ext.toLowerCase() === "json" ){
+    if (this.jsonType === 'string' && typeof this.inputData === 'string') {
+      const ext = getExt(this.inputData);
+      if (ext.toLowerCase() === 'json') {
         this._loadJson(this.inputData);
-      }else if( ext.toLowerCase() === 'fspy' ){
+      } else if (ext.toLowerCase() === 'fspy') {
         this._loadBinary();
       }
-    }else if( this.jsonType === "object" && typeof this.inputData === "object" ){
+    } else if (this.jsonType === 'object' && typeof this.inputData === 'object') {
       this.fSpyCameraData = this.inputData;
       // this._onLoadJson();
-    }else {
+    } else {
       console.error("Please put fSpy's json path or parsed json in the first argument");
     }
-
   }
-  
+
   /**
    * fSpyのjsonデータを取得する関数
    * @param {string} name カメラデータのパス
    * @return {void}
    */
-  _loadJson( path: string ): void {
+  _loadJson(path: string): void {
     const asyncFunctions = new AsyncFunctions();
-    asyncFunctions.open( path ,  this._onLoadJson.bind(this) );
+    asyncFunctions.open(path, this._onLoadJson.bind(this));
   }
 
   /**
@@ -176,24 +167,21 @@ export class FSpyCamera{
     // })
   }
 
-
   /**
    * fSpyのjsonデータを読み込んだあとに実行される関数
    * @return {void}
    */
-  _onLoadJson( result: FSpyCameraJson ): void {
-
+  _onLoadJson(result: FSpyCameraJson): void {
     this.fSpyCameraData = result;
 
-    if( this.fSpyCameraData != null ){
+    if (this.fSpyCameraData != null) {
       this.cameraTransforms = this.fSpyCameraData.cameraTransform.rows;
     }
     this.fspyImageRatio = this._getFSpyImageRatio();
     this._setMatrix();
     this._createCamera();
-    window.addEventListener('resize',this.onResize.bind(this));
+    window.addEventListener('resize', this.onResize.bind(this));
     this._runCallback.bind(this)();
-
   }
 
   /**
@@ -201,46 +189,44 @@ export class FSpyCamera{
    * @return {void}
    */
   _onLoadBinary(): void {
-
     // this.cameraTransforms = this.fSpyCameraData.cameraTransform.rows;
     // this.fspyImageRatio = this._getFSpyImageRatio();
     // this._setMatrix();
     // this._createCamera();
     // window.addEventListener('resize',this.onResize.bind(this));
     // this._runCallback.bind(this)();
-
   }
 
   /**
-   * マトリックスオブジェクトに数字をセットする 
+   * マトリックスオブジェクトに数字をセットする
    * @return {THREE.Matrix4} パラメータがセットされたMatrix4を返す
-   */ 
-  _setMatrix(): THREE.Matrix4 {
+   */
 
+  _setMatrix(): THREE.Matrix4 {
     /**
      * fSpyのカメラのtransformの配列データ
      * @type {Array}
      */
     const mtxArray = this.cameraTransforms;
 
-    const preArray : number[] = [];
+    const preArray: number[] = [];
 
     /**
-     * fSpyの多次元配列を一次元配列に変換 
+     * fSpyの多次元配列を一次元配列に変換
      * @type {Array}
-     */ 
-    const matrixArray = mtxArray.reduce((pre,curernt) => {
+     */
+
+    const matrixArray = mtxArray.reduce((pre, curernt) => {
       pre.push(...curernt);
       return pre;
-    },preArray);
+    }, preArray);
     this.rotationMatrix.elements = matrixArray;
 
     return this.rotationMatrix;
-
   }
 
   /**
-   * windowのアスペクト比を取得する関数 
+   * windowのアスペクト比を取得する関数
    * @return {number}
    */
   _getWinRatio(): number {
@@ -253,27 +239,22 @@ export class FSpyCamera{
   /**
    * fSpyのJSONから取得したもともとの画像のアスペクト比を返す
    * @return {number}
-   */ 
+   */
+
   _getFSpyImageRatio(): number {
-
-    if( this.fSpyCameraData != null ){
-
+    if (this.fSpyCameraData != null) {
       const w = this.fSpyCameraData.imageWidth;
       const h = this.fSpyCameraData.imageHeight;
       return w / h;
-
     }
 
     return 0;
-
   }
 
-  _getVFovDegFromRad( radians: number ): number {
-
-    const rad = THREE.MathUtils.radToDeg( radians );
+  _getVFovDegFromRad(radians: number): number {
+    const rad = THREE.MathUtils.radToDeg(radians);
 
     return rad;
-
   }
 
   /**
@@ -281,29 +262,25 @@ export class FSpyCamera{
    * @return {void}
    */
   _createCamera(): void {
-
-    if( this.cameraTransforms != null && this.fSpyCameraData != null ){
-      
+    if (this.cameraTransforms != null && this.fSpyCameraData != null) {
       /**
        * fSpyのカメラのtransformの配列データ
        * @type {Array}
        */
       const mtxArray = this.cameraTransforms;
-  
-      this.cameraFov = this._getVFovDegFromRad( this.fSpyCameraData.verticalFieldOfView );
-  
+
+      this.cameraFov = this._getVFovDegFromRad(this.fSpyCameraData.verticalFieldOfView);
+
       // this.camera = new THREE.PerspectiveCamera( this.cameraFov , this.canvasWidth / this.canvasHeight , 0.01 , 10000 );
 
       this.camera.fov = this.cameraFov;
       this.camera.aspect = this.canvasWidth / this.canvasHeight;
       this.camera.near = 0.01;
       this.camera.far = 10000;
-      this.camera.position.set( mtxArray[0][3] , mtxArray[1][3] , mtxArray[2][3] );
-      this.camera.setRotationFromMatrix( this.rotationMatrix );
+      this.camera.position.set(mtxArray[0][3], mtxArray[1][3], mtxArray[2][3]);
+      this.camera.setRotationFromMatrix(this.rotationMatrix);
       this.initCameraAspect = this.camera.aspect;
-
     }
-    
   }
 
   /**
@@ -311,32 +288,29 @@ export class FSpyCamera{
    * @return {void}
    */
   onResize(): void {
-
     // this.canvasWidth = window.innerWidth;
     // this.canvasHeight = window.innerHeight;
     this.canvasWidth = window.innerWidth;
-    this.canvasHeight = 500
+    this.canvasHeight = 500;
     // if( this.canvasWidth / this.canvasHeight <= this.initCameraAspect  ){
-    if( this.canvasWidth / this.canvasHeight <= this._getFSpyImageRatio()  ){
-        this.camera.aspect = this.canvasWidth /  this.canvasHeight;
-        this.camera.zoom = 1;
-    }else{
-        this.camera.aspect = this.canvasWidth /  this.canvasHeight;
-        // this.camera.zoom = this.canvasWidth /  this.canvasHeight / this.initCameraAspect;
-        this.camera.zoom = this.canvasWidth /  this.canvasHeight / this._getFSpyImageRatio();
+    if (this.canvasWidth / this.canvasHeight <= this._getFSpyImageRatio()) {
+      this.camera.aspect = this.canvasWidth / this.canvasHeight;
+      this.camera.zoom = 1;
+    } else {
+      this.camera.aspect = this.canvasWidth / this.canvasHeight;
+      // this.camera.zoom = this.canvasWidth /  this.canvasHeight / this.initCameraAspect;
+      this.camera.zoom = this.canvasWidth / this.canvasHeight / this._getFSpyImageRatio();
     }
     this.camera.updateProjectionMatrix();
   }
 
   /**
-   * コールバックを実行する関数 
+   * コールバックを実行する関数
    * @return {void}
    */
   _runCallback(): void {
-
-    if( getType( this.callback ) === "function" ){
+    if (getType(this.callback) === 'function') {
       this.callback(this);
     }
-
   }
 }
