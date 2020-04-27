21dc8653184c7db8f7c0a7f8482fbde9e50625b4
youkan <youkan94@gmail.com>
2020-04-28 02:09:31 +0900

    style : prettier + eslint

--- src/AsyncFunctions.ts
+++ src/AsyncFunctions.ts
@@ -1,19 +1,18 @@
-import { FSpyCameraJson } from "type";
+import { FSpyCameraJson } from 'type';
 
-export default class AsyncFunctions{
-
+export default class AsyncFunctions {
   protected xhr: XMLHttpRequest;
 
-  constructor(){
+  constructor() {
     this.xhr = new XMLHttpRequest();
     this.xhr.responseType = 'json';
   }
 
-  open( path: string , callback: ( result : FSpyCameraJson ) => void ): void{
-    this.xhr.open("GET",path);
-    this.xhr.addEventListener('load', function(  ){
+  open(path: string, callback: (result: FSpyCameraJson) => void): void {
+    this.xhr.open('GET', path);
+    this.xhr.addEventListener('load', function() {
       const result = this.response;
-      callback( result );
+      callback(result);
     });
     this.xhr.send();
   }
