f098ffff6cec519b25125b3fee95c54e4f2ca0fc
youkan <youkan94@gmail.com>
2020-04-27 23:33:56 +0900

    feat : 非同期処理を外部クラス化

--- src/AsyncFunctions.ts
+++ src/AsyncFunctions.ts
@@ -1,5 +1,20 @@
-export default class AsyncFunctions{
-  constructor(){
+import { FSpyCameraJson } from "type";
 
+export default class AsyncFunctions{
+
+  protected xhr: XMLHttpRequest;
+
+  constructor(){
+    this.xhr = new XMLHttpRequest();
+    this.xhr.responseType = 'json';
+  }
+
+  open( path: string , callback: ( result : FSpyCameraJson ) => void ): void{
+    this.xhr.open("GET",path);
+    this.xhr.addEventListener('load', function(  ){
+      const result = this.response;
+      callback( result );
+    });
+    this.xhr.send();
   }
 }
