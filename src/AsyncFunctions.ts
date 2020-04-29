ea289a18541ef219db8b2d0bccc5cf9841742d4c
youkan <youkan94@gmail.com>
2020-04-29 12:21:57 +0900

    fix : IEで動くように修正

--- src/AsyncFunctions.ts
+++ src/AsyncFunctions.ts
@@ -1,17 +1,20 @@
 import { FSpyCameraJson } from 'type';
+import getBrowserName from './utils/getBrowserName';
 
 export default class AsyncFunctions {
   protected xhr: XMLHttpRequest;
 
   constructor() {
     this.xhr = new XMLHttpRequest();
-    this.xhr.responseType = 'json';
   }
 
   open(path: string, callback: (result: FSpyCameraJson) => void): void {
     this.xhr.open('GET', path);
+    this.xhr.responseType = 'json';
     this.xhr.addEventListener('load', function () {
-      const result = this.response;
+      const browserName = getBrowserName();
+
+      const result = browserName === 'ie' ? JSON.parse(this.response) : this.response;
       callback(result);
     });
     this.xhr.send();
