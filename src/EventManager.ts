e101e0ae111994537a36372ff479e3a6a6ca1217
youkan <youkan94@gmail.com>
2020-05-14 01:08:03 +0900

    refactor: delete unused class , function , variable

--- src/EventManager.ts
+++
@@ -1,3 +0,0 @@
-export default class EventManager {
-  constructor() {}
-}
--- src/utils/getExt.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ファイルの拡張子を返す関数
- */
-export function getExt(filename: string): string {
-  const dotPosition = filename.lastIndexOf('.');
-  if (dotPosition === -1) return '';
-  return filename.slice(dotPosition + 1);
-}
