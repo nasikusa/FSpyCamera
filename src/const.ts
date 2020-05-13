ce22091a6f82c7ba6fb773a0ee4abf8240f0149f
youkan <youkan94@gmail.com>
2020-05-13 21:39:02 +0900

    refactor: add error text data

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,6 @@
 export const NAME = 'FSpyCamera';
 export const VERSION = '1.0.0';
+
+export const ErrorTexts: { [key: string]: string } = {
+  loadError: "Please put fSpy's json path or parsed json in the first argument",
+};
