6bbd35288b375835a35e0483d5cf6ebb4fd1c55e
youkan <youkan94@gmail.com>
2020-05-16 18:49:56 +0900

    chore: add build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/index.ts',
@@ -31,3 +32,31 @@ export default {
 		typescript( { typescript: require( 'typescript' ) } ),
 	],
 };
+
+const build = {
+  input: 'src/index.ts',
+	external: [
+		'three',
+	],
+	output: [
+		{
+			format: 'umd',
+			name: 'FSpyCameraLoader',
+			file: pkg.mainMin,
+			banner: license,
+			indent: '\t',
+			globals: {
+				three: 'THREE',
+			}
+		},
+	],
+	plugins: [
+		typescript( { typescript: require( 'typescript' ) } ),
+		terser({ output: { comments: /@license/i } }),
+	],
+};
+
+export default [
+	develop,
+	build,
+];
