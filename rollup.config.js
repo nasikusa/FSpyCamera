c3e4d02ec9c5d3cbd4746de2fa2e1889e34bbfea
youkan <youkan94@gmail.com>
2020-05-31 10:06:54 +0900

    chore : tab -> space

--- rollup.config.js
+++ rollup.config.js
@@ -12,60 +12,60 @@ const license = `/*!
 const develop = {
   input: 'src/index.ts',
   external: [
-		'three',
-	],
-	output: [
-		{
-			format: 'umd',
-			name: 'FSpyCameraLoader',
-			file: pkg.main,
-			banner: license,
-			indent: '\t',
-			globals: {
-				three: 'THREE',
-			},
-			// exports: 'named',
-			// extend: true,
-		},
-		{
-			format: 'es',
-			file: pkg.module,
-			banner: license,
-			indent: '\t',
-			globals: {
-				three: 'THREE',
-			}
-		}
-	],
-	plugins: [
-		typescript( { typescript: require( 'typescript' ) } ),
-	],
+    'three',
+  ],
+  output: [
+    {
+      format: 'umd',
+      name: 'FSpyCameraLoader',
+      file: pkg.main,
+      banner: license,
+      indent: '\t',
+      globals: {
+        three: 'THREE',
+      },
+      // exports: 'named',
+      // extend: true,
+    },
+    {
+      format: 'es',
+      file: pkg.module,
+      banner: license,
+      indent: '\t',
+      globals: {
+        three: 'THREE',
+      }
+    }
+  ],
+  plugins: [
+    typescript( { typescript: require( 'typescript' ) } ),
+  ],
 };
 
 const build = {
   input: 'src/index.ts',
-	external: [
-		'three',
-	],
-	output: [
-		{
-			format: 'umd',
-			name: 'FSpyCameraLoader',
-			file: pkg.mainMin,
-			banner: license,
-			indent: '\t',
-			globals: {
-				three: 'THREE',
-			}
-		},
-	],
-	plugins: [
-		typescript( { typescript: require( 'typescript' ) } ),
-		terser({ output: { comments: /@license/i } }),
-	],
+  external: [
+    'three',
+  ],
+  output: [
+    {
+      format: 'umd',
+      name: 'FSpyCameraLoader',
+      file: pkg.mainMin,
+      banner: license,
+      indent: '\t',
+      globals: {
+        three: 'THREE',
+      }
+    },
+  ],
+  plugins: [
+    typescript( { typescript: require( 'typescript' ) } ),
+    terser({ output: { comments: /@license/i } }),
+  ],
 };
 
 export default [
-	develop,
-	build,
+  develop,
+  build,
 ];
